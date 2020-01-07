c3e87ea1075333bc4beca771a259eb6e7fdd9dd4
Josefine2020 <vilma.tayun931@gmail.com>
2020-01-07 02:23:08 +0000

    we did some div and we create some loops

---
+++ js/script.js
@@ -0,0 +1,26 @@
+let songNames = ["boo'd up", 'dreams'];
+
+songNames.forEach(function(song){
+    $('#songNames').append(song + ' ')
+
+})
+
+let artistNames =['artist1', 'artist2'];
+artistNames.forEach(function(name){
+$('#artistNames').append(name + ' ')
+
+})
+
+let image=[' ', ' '];
+image.forEach(function(image){
+$('#image').append(image + ' ')
+
+})
+
+let link =['link', ' '];
+link.forEach(function(link){
+$('#link').append(link + ' ')
+
+})
+       
+
