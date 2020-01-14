6a089944bb417e668449858cc222305a7b581c8d
Josefine2020 <vilma.tayun931@gmail.com>
2020-01-14 02:23:45 +0000

    day 2, worked on append

--- js/script.js
+++ js/script.js
@@ -1,26 +1,37 @@
 let songNames = ["boo'd up", 'dreams'];
-
-songNames.forEach(function(song){
-    $('#songNames').append(song + ' ')
-
-})
-
 let artistNames =['artist1', 'artist2'];
-artistNames.forEach(function(name){
-$('#artistNames').append(name + ' ')
+let imagesLink=[' ', ' '];
+let links =['link', ' '];
+$('.addButton').click(function() {
 
+
+    let songName = $(".songName").val();
+let artistName = $(".artistName").val();
+let imageLink = $(".imageLink").val();
+$(".addButton").empty();
+ 
+songNames.push(songName);
+artistNames.push(artistName);
+images.push(imageLink)
+
+ songNames.forEach(function(song){
+        $('#songNames').append(song + ' ')
+    })
+
+    artistNames.forEach(function(name){
+    $('#artistNames').append(name + ' ')
+
+    })
+
+    imageLink.forEach(function(image){
+    $('#image').append(image + ' ')
+    })
+
+
+    link.forEach(function(link){
+    $('#link').append(link + ' ')
+
+    })
 })
 
-let image=[' ', ' '];
-image.forEach(function(image){
-$('#image').append(image + ' ')
-
-})
-
-let link =['link', ' '];
-link.forEach(function(link){
-$('#link').append(link + ' ')
-
-})
-       
 
