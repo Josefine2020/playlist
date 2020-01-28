63f8153f7073d4083d6880c533eec7a1938016e1
Josefine2020 <vilma.tayun931@gmail.com>
2020-01-28 02:28:13 +0000

    I finish some objectsa

--- js/script.js
+++ js/script.js
@@ -1,37 +1,50 @@
-let songNames = ["boo'd up", 'dreams'];
-let artistNames =['artist1', 'artist2'];
-let imagesLink=[' ', ' '];
-let links =['link', ' '];
+
+// let songNames = ["boo'd up", 'memories', 'shallow'];
+// let artistNames =['Alla Mai1', 'Marron2', 'Lady Gaga'];
+// let Link=[' https://www.youtube.com/watch?v=6YNZlXfW6Ho', 'https://www.youtube.com/watch?v=SlPhMPnQ58k ', 'https://www.youtube.com/watch?v=bo_efYhYU2A'];
+// let imagesLinks =['link', ' '];
+
+let songInfo = {
+    songName: "yesterday",
+    artistName: "the beatles",
+    imagesLink: "www.fakewebsite.com",
+    Link: ""
+}
+
+let songs = [songInfo]
+
 $('.addButton').click(function() {
 
 
-    let songName = $(".songName").val();
-let artistName = $(".artistName").val();
-let imageLink = $(".imageLink").val();
-$(".addButton").empty();
+
+songs.forEach(function(song){
+    $('#songNames').append(song.songName + ' ')
+})
+    // let songName = $(".songName").val();
+// let artistName = $(".artistName").val();
+// let imageLink = $(".imageLink").val();
+// $(".addButton").empty();
  
-songNames.push(songName);
-artistNames.push(artistName);
-images.push(imageLink)
+// songNames.push(songName);
+// artistNames.push(artistName);
+// images.push(imageLink)
 
- songNames.forEach(function(song){
-        $('#songNames').append(song + ' ')
-    })
+//  
 
-    artistNames.forEach(function(name){
-    $('#artistNames').append(name + ' ')
+//     artistNames.forEach(function(name){
+//     $('#artistNames').append(name + ' ')
 
-    })
+//     })
 
-    imageLink.forEach(function(image){
-    $('#image').append(image + ' ')
-    })
+//     imageLink.forEach(function(image){
+//     $('#image').append(image + ' ')
+//     })
 
 
-    link.forEach(function(link){
-    $('#link').append(link + ' ')
+//     link.forEach(function(link){
+//     $('#link').append(link + ' ')
 
-    })
+//     })
 })
 
 
